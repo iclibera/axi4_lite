b3f57efa3462e826b6b6f7c0e0cdfadf4b8eb2da
Bera Icli <iclibera@gmail.com>
2024-06-20 00:23:21 +0100

    svunit.f modified

--- sim/svunit.f
+++ sim/svunit.f
@@ -1,3 +1,11 @@
-../../common/glbl.v"
-../../common/axi_vip/hdl/*
-../../hdl/axi4_lite_sub.sv
+../common/glbl.v
+../common/axi_vip/hdl/xil_common_vip_macros.svh
+../common/axi_vip/hdl/xil_common_vip_pkg.sv
+../common/axi_vip/hdl/axi_vip_pkg.sv
+../common/axi_vip/hdl/axi_vip_axi4pc.sv
+../common/axi_vip/hdl/axi_vip_if.sv
+../common/axi_vip/hdl/axi_vip_v1_1_vl_rfs.sv
+../common/axi_vip/hdl/vip_axi_vip_0_pkg.sv
+../common/axi_vip/hdl/vip_axi_vip_0.sv
+../hdl/axi4_lite_sub.sv
+tb01_unit_test.sv
